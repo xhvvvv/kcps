ae9bc39d2bc891240332b6d31f469c915b17862e
xhvvvv <wj1939146725@gmail.com>
2024-03-17 23:18:02 +0800

    frp代码部分
    Signed-off-by: xhvvvv <wj1939146725@gmail.com>

--- frpfun/src/main/java/com/wxfactory/kcps/frpfun/entity/FrpConfigS.java
+++ frpfun/src/main/java/com/wxfactory/kcps/frpfun/entity/FrpConfigS.java
@@ -2,6 +2,10 @@ package com.wxfactory.kcps.frpfun.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.InetSocketAddress;
 
 @Getter
 @Setter
@@ -9,4 +13,12 @@ public class FrpConfigS extends FrpConfig{
     private String kcpBindPort;
     private String quicBindPort;
     private String tcpmuxHTTPConnectPort ;
+
+    public FrpConfigS(@NotNull InetSocketAddress publicConnect) {
+        super(publicConnect);
+    }
+
+    public FrpConfigS(@NotNull InetSocketAddress publicConnect, @Nullable Authentication authentication) {
+        super(publicConnect, authentication);
+    }
 }
