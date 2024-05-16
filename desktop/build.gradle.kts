219d538a6d141c685888e8022c72e11fa697aafd
xhvvvv <wj1939146725@gmail.com>
2024-05-16 11:52:08 +0800

    这混淆是啥玩意

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -12,23 +12,33 @@ tasks.compileJava{
     this.options.encoding = "utf-8"
 }
  
-
+version = "1.0.0"
 compose.desktop {
     
     application {
 //        this.jvmArgs.add()
         mainClass = "com.wxfactory.kcps.desktop.MainKt"
         nativeDistributions {
+            this.licenseFile .set(rootProject.file("LICENSE"))
             targetFormats(
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg,
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb,
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Rpm
             )
-            packageName = "frpClient"
-            packageName = "1.0.0"
+            this.packageName = "Fcp"
+            this.packageVersion = version.toString()
+            windows{
+                this.packageVersion = version.toString()
+                this.iconFile.set(file("icon.svg"))
+            }
         }
-        
+        buildTypes{
+            release{
+                proguard{
+                    configurationFiles.from("proguard.pro")
+                }
+            }
+        }
     }
-    
 }
